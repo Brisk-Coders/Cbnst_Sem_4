acf340cae9f27207704c8e33b1f94c0f03c55ca9
Ayush Verma <54748755+AyushhVerma@users.noreply.github.com>
2021-06-08 16:43:12 +0530

    Add files via upload

---
+++ newtonraph.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#define f(x) (3*x - sin(x) + 1)
+#define g(x) (3 - cos(x))
+
+int main()
+{
+    float x0, e, x;
+    int i, c = 0;
+    printf("Enter Initial guess and maximum tolerable error and maximum iterations\n");
+    scanf("%f%f%d", &x0, &e, &i);
+    if(g(x0) == 0) {
+        printf("Error");
+        return 0;
+    }
+    do {
+        x = x0 - f(x0)/g(x0);
+        x0 = x;
+        if(++c > i) {
+            printf("Not convergent");
+            return 0;
+        }
+    }while(fabs(f(x)) > e);
+    printf("%f", x);
+    return 0;
+}

02f4692c832811ac320552eb72238227a2cab8e2
Abhay Pratap Singh <apsr0316@gmail.com>
2021-06-13 19:35:11 +0530

    Update NewtonBackwardInterpolation.c

--- NewtonBackwardInterpolation.c
+++ NewtonBackwardInterpolation.c
@@ -1,84 +1,44 @@
 #include<stdio.h>
-#include<conio.h>
-#include<math.h>
-void main()
-{
-  float x[10],y[10][10],sum,p,u,temp;
-  int i,n,j,k=0,f,m;
 
-  float fact(int);
-  clrscr();
-  printf("\n Enter the number of records you want to enter: ");
-  scanf("%d",&n);
-  for(i=0; i<n; i++)
-  {
-   printf("\n\nenter the value of x%d: ",i);
-   scanf("%f",&x[i]);
-   printf("\n\nenter the value of f(x%d): ",i);
-   scanf("%f",&y[k][i]);
-  }
-  printf("\n\nEnter X for finding f(x): ");
-  scanf("%f",&p);
-
-  for(i=1;i<n;i++)
-  {
-    for(j=i;j<n;j++)
-    {
-     y[i][j]=y[i-1][j]-y[i-1][j-1];
-    }
-  }
-
-  printf("\n_____________________________________________________\n");
-  printf("\n  x(i)\t   y(i)\t    y1(i)    y2(i)    y3(i)    y4(i)");
-  printf("\n_____________________________________________________\n");
-  
-  for(i=0;i<n;i++)
-  {
-    printf("\n %.3f",x[i]);
-    for(j=0;j<=i;j++)
-    {
-     printf("   ");
-     printf(" %.3f",y[j][i]);
-    }
-   printf("\n");
-  }
-
-  i=0;
-  do
-  {
-   if(x[i]<p && p<x[i+1])
-    k=1;
-   else
-    i++;
-  }while(k != 1);
-  f=i+1;
-  u=(p-x[f])/(x[f]-x[f-1]);
-  printf("\n\n u = %.3f ",u);
-
-  n=n-i+1;
-  sum=0;
-  for(i=0;i<n;i++)
-  {
-   temp=1;
-   for(j=0;j<i;j++)
-   {
-    temp = temp * (u + j);
-   }
-    m=fact(i);
-    sum = sum + temp*(y[i][f]/m);
-  }
-  printf("\n\n f(%.2f) = %f ",p,sum);
-  getch();
+float u_cal(float u, int n){
+    float t = u;
+    for(int i=1;i<n;i++)    
+        t *= (u+i);
+    return t;
 }
+ int fact(int n){
+    if(n==0) return 1;
+    return n*fact(n-1);
+ }
 
-float fact(int a)
-{
-  float fac = 1;
+int main(){
+    int n;
+    printf("Enter n: ");
+    scanf("%d",&n);
+    float y[10][10];
+    float x[10];
+    printf("Enter the values of x argument:\n");
+    for(int i=0;i<n;i++)
+        scanf("%f",&x[i]);
+    printf("Enter the values of y argument:\n");
+    for(int i=0;i<n;i++)
+        scanf("%f",&y[i][0]);
+    for(int i=1;i<n;i++)
+        for(int j=n-1;j>=i;j--)  
+            y[j][i] = y[j][i-1] - y[j-1][i-1];
+    for(int i=0;i<n;i++){
+        printf("%.4f\t",x[i]);
+        for(int j=0;j<=i;j++)
+            printf("%.4f\t",y[i][j]);
+        printf("\n");
+    }
+    float v;
+    printf("Enter the value to interpolate: ");
+    scanf("%f",&v);
+    float sum=y[n-1][0];
+    float u = (v-x[n-1])/(x[1] - x[0]);
+    for(int i=1;i<n;i++)    
+        sum += (u_cal(u,i)*y[n-1][i])/fact(i);
 
-  if (a == 0)
-   return (1);
-  else
-   fac = a * fact(a-1);
-
-  return(fac);
+    printf("val at %f is %.4f",v,sum);
 }

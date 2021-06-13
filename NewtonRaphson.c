6d12eb958dd651de496e96292ff82fbb47765731
Ayush Verma <54748755+AyushhVerma@users.noreply.github.com>
2021-06-13 18:37:20 +0530

    Update NewtonRaphson.c

--- NewtonRaphson.c
+++ NewtonRaphson.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <math.h>
-#define f(x) (3*x - sin(x) + 1)
+#define f(x) (3*x - sin(x) - 1)
 #define g(x) (3 - cos(x))
 
 int main()
 {
     float x0, e, x;
     int i, c = 0;
-    printf("Enter Initial guess and maximum tolerable error and maximum iterations\n");
+    printf("Enter Initial guess and maximum tolerable error and maximum iterations\n"); // 1 .0001 10
     scanf("%f%f%d", &x0, &e, &i);
     if(g(x0) == 0) {
         printf("Error");
@@ -21,6 +21,6 @@ int main()
             return 0;
         }
     }while(fabs(f(x)) > e);
-    printf("%f", x);
+    printf("%f", x); // 0.490296
     return 0;
 }
